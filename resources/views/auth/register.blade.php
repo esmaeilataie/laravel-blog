6ce4b00bb7f0d254eff2ee4a023b0e23456ca729
Esmaeil Ataie <esmaeilataie@yahoo.com>
2025-12-04 22:17:57 +0330

    work on registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,36 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<x-layout page_title="ثبت نام">
+    <main class="bg--white">
+        <div class="container">
+            <div class="sign-page">
+                <h1 class="sign-page__title">ثبت نام در وب‌سایت</h1>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+
+                <form class="sign-page__form" method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <input name="name" value="{{ old('name') }}" type="text" class="text text--right"
+                           placeholder="نام و نام خانوادگی">
+                    <input type="text" name="mobile" value="{{ old('mobile') }}" class="text text--left"
+                           placeholder="شماره موبایل">
+                    <input type="text" name="email" value="{{ old('email') }}" class="text text--left"
+                           placeholder="ایمیل">
+                    <input type="password" name="password" class="text text--left" placeholder="رمز عبور">
+                    <input type="password" name="password_confirmation" class="text text--left"
+                           placeholder="تکراررمزعبور">
+
+
+                    <button class="btn btn--blue btn--shadow-blue width-100 mb-10">ثبت نام</button>
+                    <div class="sign-page__footer">
+                        <span>در سایت عضوید ؟ </span>
+                        <a href="{{ route('login') }}" class="color--46b2f0">صفحه ورود</a>
+
+                    </div>
+                </form>
+            </div>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </main>
+</x-layout>
