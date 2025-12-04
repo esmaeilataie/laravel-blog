16c76950c22e33f56a8d9a2857a87318de7fb6dd
Esmaeil Ataie <esmaeilataie@yahoo.com>
2025-12-04 18:18:59 +0330

    create some blades and add some routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing');
-});
+})->name('home');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('/reset-password', function () {
+    return view('reset-password');
+})->name('reset-password');
+
+Route::get('/posts/{id}',function(){
+    return view('post');
+})->name('post.show');
