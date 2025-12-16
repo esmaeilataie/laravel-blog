2e5a91ae6745fe290a83a785d04873442a39ed0a
Esmaeil Ataie <esmaeilataie@yahoo.com>
2025-12-16 21:03:00 +0330

    make user's create page in admin panel

--- app/Http/Controllers/Panel/UserController.php
+++ app/Http/Controllers/Panel/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Panel;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -22,7 +25,16 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'persian_alpha','max:255'],
+            'email' => ['required', 'email', 'lowercase', 'string', 'unique:'.User::class],
+            'mobile' => ['required','string', 'max:255', 'unique:'.User::class,'ir_mobile'],
+            'role' => ['required', Rule::in(['user', 'author', 'admin'])]
+        ]);
+
+        $validated['password'] = Hash::make('password');
+        User::create($validated);
+        return redirect()->route('users.index');
     }
 
 
--- resources/views/panel/users/create.blade.php
+++ resources/views/panel/users/create.blade.php
@@ -10,17 +10,26 @@
         <div class="row no-gutters  bg-white">
             <div class="col-12">
                 <p class="box__title">ساخت کاربر</p>
-                <form action="" class="padding-30" method="post">
-                    <input type="text" class="text" placeholder="نام و نام خانوادگی">
-                    <input type="text" class="text" placeholder="ایمیل">
-                    <input type="text" class="text" placeholder="شماره موبایل">
-                    <input type="text" class="text" placeholder="آی پی" value="102.111.11" disabled>
-                    <select name="" id="">
-                        <option value="0">کاربر عادی</option>
-                        <option value="1">مدرس</option>
-                        <option value="2">نویسنده</option>
-                        <option value="3">مدیر</option>
+                <form action="{{ route('users.store') }}" class="padding-30" method="POST">
+                    @csrf
+                    <input type="text" value="{{ old('name') }}"  name="name" class="text"
+                           placeholder="نام">
+                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+
+                    <input type="email"value="{{ old('email') }}"  name="email" class="text" placeholder="ایمیل">
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
+                    <input type="text" value="{{ old('mobile') }}" name="mobile" class="text"
+                           placeholder="شماره موبایل">
+                    <x-input-error :messages="$errors->get('mobile')" class="mt-2" />
+
+                    <select name="role" id="">
+                        <option value="user" selected>کاربر عادی</option>
+                        <option value="author">نویسنده</option>
+                        <option value="admin">مدیر</option>
                     </select>
+                    <x-input-error :messages="$errors->get('role')" class="mt-2" />
+
                     <button class="btn btn-webamooz_net">ساخت</button>
                 </form>
 
