a71d18fdde39f3674c7d4ce909df14d1ec5a20fa
Esmaeil Ataie <esmaeilataie@yahoo.com>
2025-12-24 20:16:54 +0330

    create posts and category_post pivot table migrations

--- database/migrations/2025_12_23_202052_create_posts_table.php
+++ database/migrations/2025_12_23_202052_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('banner');
+            $table->longText('content');
             $table->timestamps();
         });
     }
